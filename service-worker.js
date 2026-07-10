3ee102e5046c7f690b2b1ac253e4b3e9fa1fd294
praisefxtech <praisekenechukwuchigbo@gmail.com>
2026-07-10 11:29:18 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,74 +1,83 @@
-const CACHE_NAME = "studymate-v7";
+const CACHE_NAME = "studymate-v8";
 
 const FILES_TO_CACHE = [
+  "./",
+  "index.html",
+  "style.css",
+  "main.js",
+  "manifest.json",
+  "privacy.html",
 
-"index.html",
-"main.js",
-"style.css",
-"manifest.json",
+  "icon-192.png",
+  "icon-512.png",
 
-"icon-192.png",
-"icon-512.png",
+  "notification.mp3",
 
-
-// notification sound
-"notification.mp3",
-
-
-// focus music
-"calm.mp3",
-"dream.mp3",
-"sky.mp3",
-"storm.mp3",
-"piano.mp3",
-"ocean.mp3",
-"space.mp3",
-"silent.mp3"
-
+  "calm.mp3",
+  "dream.mp3",
+  "sky.mp3",
+  "storm.mp3",
+  "piano.mp3",
+  "ocean.mp3",
+  "space.mp3",
+  "silent.mp3"
 ];
 
 
-self.addEventListener("install", (event) => {
+// INSTALL
+self.addEventListener("install", event => {
 
-    self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(FILES_TO_CACHE);
-        })
-    );
-
-});
-
-
-self.addEventListener("activate", (event) => {
-
-    event.waitUntil(
-        caches.keys().then((keys) => {
-            return Promise.all(
-                keys.map((key) => {
-                    if (key !== CACHE_NAME) {
-                        return caches.delete(key);
-                    }
-                })
-            );
-        }).then(() => self.clients.claim())
-    );
+  self.skipWaiting();
 
 });
 
 
-self.addEventListener("fetch", function(event){
+// ACTIVATE
+self.addEventListener("activate", event => {
 
-    event.respondWith(
+  event.waitUntil(
 
-        caches.match(event.request)
-        .then(function(response){
+    caches.keys().then(keys => {
 
-            return response || fetch(event.request);
+      return Promise.all(
+
+        keys.map(key => {
+
+          if(key !== CACHE_NAME){
+            return caches.delete(key);
+          }
 
         })
 
-    );
+      );
+
+    }).then(() => self.clients.claim())
+
+  );
+
+});
+
+
+// FETCH
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+    .then(response => {
+
+      return response || fetch(event.request)
+      .catch(() => caches.match("index.html"));
+
+    })
+
+  );
 
 });
